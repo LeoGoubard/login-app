707d3be436a83abe78d0eb26070d404eef8f86cc
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-05 16:02:38 +0200

    :construction:profile done, wip backend

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,15 +1,15 @@
 import toast from 'react-hot-toast'
-// import { authenticate } from './helper'
+import { authenticate } from './helper'
 
 export const usernameValidate = async(values) => {
     const errors = usernameVerify({}, values);
 
     if(values.username){
-        // const { status } = await authenticate(values.username);
+        const { status } = await authenticate(values.username);
         
-        /* if(status !== 200){
+        if(status !== 200){
             errors.exist = toast.error('User does not exist...!')
-        } */
+        }
     }
 
     return errors;
