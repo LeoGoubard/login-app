72dfc57ddadc12577e24f6a51aad9f14f751ad7a
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-09 15:26:16 +0200

    :construction: ready to deploy

--- client/src/components/Reset/index.js
+++ client/src/components/Reset/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styles from "../../styles/Username.module.css";
 import toast, { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
@@ -12,7 +12,7 @@ import useFetch from '../../hooks/fetch.hook';
 const Reset = () => {
   const { username } = useAuthStore(state => state.auth);
   const navigate = useNavigate();
-  const [{ isLoading, apiData, status, serverError }] = useFetch('createResetSession')
+  const [{ isLoading, status, serverError }] = useFetch('createResetSession')
 
   const formik = useFormik({
     initialValues: {
